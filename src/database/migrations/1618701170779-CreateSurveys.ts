ec6a5257a08cfb6920f94646c713df40c5d77a8f
augustopiatto <augusto.piatto@hotmail.com>
2021-04-17 21:14:49 -0300

    criacao da pesquisa no banco de dados

---
+++ src/database/migrations/1618701170779-CreateSurveys.ts
@@ -0,0 +1,36 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSurveys1618701170779 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "surveys",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "title",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "Now()",
+          },
+        ],
+      }),
+    );
+  }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
